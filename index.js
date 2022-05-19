78200126a9cf5e5eb4a9aa9c4c84bf998878a695
Bach Le <lehoangbach7802@gmail.com>
2022-05-19 11:24:33 -0400

    commit tree 3a2204fa1041b6003d2bc09286b9adc4bb1bad04

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  let newTutorials = tutorials.map(function(element){
+    return element 
+      .split(' ')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' ');
+  })
+
+  return newTutorials
 }
